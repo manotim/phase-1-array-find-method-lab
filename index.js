157b0272bc6a11e686254a7ef5f7f001f41a90a6
manoti <manotimike@gmail.com>
2023-03-21 22:21:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    let win = array.find(item => item.result === 'W')
+    if (win) { 
+    return win.year
+    } else {
+        return undefined
+    }
+}
